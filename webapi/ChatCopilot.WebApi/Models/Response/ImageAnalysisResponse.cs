98c36afce9164ff7047be0089f0d8ec367d9aa33
ShuaiHua Du <shuaihua.du@outlook.com>
2024-04-12 01:15:59 +0800

    Add image analysis response.

---
+++ webapi/ChatCopilot.WebApi/Models/Response/ImageAnalysisResponse.cs
@@ -0,0 +1,16 @@
+﻿namespace ChatCopilot.WebApi.Models.Response;
+
+public class ImageAnalysisResponse
+{
+    [JsonPropertyName("hateResult")]
+    public AnalysisResult? HateResult { get; set; }
+
+    [JsonPropertyName("selfHarmResult")]
+    public AnalysisResult? SelfHarmResult { get; set; }
+
+    [JsonPropertyName("sexualResult")]
+    public AnalysisResult? SexualResult { get; set; }
+
+    [JsonPropertyName("violenceResult")]
+    public AnalysisResult? ViolenceResult { get; set; }
+}
