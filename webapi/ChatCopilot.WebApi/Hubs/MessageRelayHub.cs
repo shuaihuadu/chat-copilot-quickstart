933f9ed431c75a85ffca98cccb66a3c813fa9c8e
ShuaiHua Du <shuaihua.du@outlook.com>
2024-04-12 01:15:20 +0800

    Add message relay hub.

---
+++ webapi/ChatCopilot.WebApi/Hubs/MessageRelayHub.cs
@@ -0,0 +1,29 @@
+﻿namespace ChatCopilot.WebApi.Hubs;
+
+public class MessageRelayHub : Hub
+{
+    private const string ReceiveMessageClientCall = "ReceiveMessage";
+    private const string ReceiveUserTypingStateClientCall = "ReceiveUserTypingState";
+
+    private readonly ILogger<MessageRelayHub> _logger;
+
+    public MessageRelayHub(ILogger<MessageRelayHub> logger)
+    {
+        this._logger = logger;
+    }
+
+    public async Task AddClientToGroupAsync(string chatId)
+    {
+        await this.Groups.AddToGroupAsync(this.Context.ConnectionId, chatId);
+    }
+
+    public async Task SendMessageAsync(string chatId, string senderId, object message)
+    {
+        await this.Clients.OthersInGroup(chatId).SendAsync(ReceiveMessageClientCall, chatId, senderId, message);
+    }
+
+    public async Task SendUserTypingStateAsync(string chatId, string userId, bool isTyping)
+    {
+        await this.Clients.OthersInGroup(chatId).SendAsync(ReceiveUserTypingStateClientCall, chatId, userId, isTyping);
+    }
+}
