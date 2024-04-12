984bc636590e3921ed726e76cc52567c97152e53
ShuaiHua Du <shuaihua.du@outlook.com>
2024-04-12 17:51:36 +0800

    Service configurations completed.

--- shared/ChatCopilot.Shared/ServiceConfiguration.cs
+++ shared/ChatCopilot.Shared/ServiceConfiguration.cs
@@ -1,8 +1,13 @@
 ﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.KernelMemory;
+using Microsoft.KernelMemory.AI;
 using Microsoft.KernelMemory.Configuration;
 using Microsoft.KernelMemory.ContentStorage.DevTools;
+using Microsoft.KernelMemory.MemoryStorage;
+using Microsoft.KernelMemory.MemoryStorage.DevTools;
 using Microsoft.KernelMemory.Pipeline.Queue.DevTools;
+using Microsoft.KernelMemory.Postgres;
 
 namespace ChatCopilot.Shared;
 
@@ -80,6 +85,194 @@ internal sealed class ServiceConfiguration
         this.ConfigureImageOCR(builder);
 
         return builder;
+    }
+
+    private void ConfigureImageOCR(IKernelMemoryBuilder builder)
+    {
+        switch (this._kernelMemoryConfig.DataIngestion.ImageOcrType)
+        {
+            case string x when string.IsNullOrWhiteSpace(x):
+            case string y when y.Equals("None", StringComparison.OrdinalIgnoreCase):
+                break;
+            case string x when x.Equals("AzureAIDocIntel", StringComparison.OrdinalIgnoreCase):
+                builder.Services.AddAzureAIDocIntel(this.GetServiceConfig<AzureAIDocIntelConfig>("AzureAIDocIntel"));
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void ConfigureTextGenerator(IKernelMemoryBuilder builder)
+    {
+        switch (this._kernelMemoryConfig.TextGeneratorType)
+        {
+            case string x when x.Equals("AzureOpenAI", StringComparison.OrdinalIgnoreCase):
+            case string y when y.Equals("AzureOpenAIText", StringComparison.OrdinalIgnoreCase):
+                builder.Services.AddAzureOpenAITextGeneration(this.GetServiceConfig<AzureOpenAIConfig>("AzureOpenAIText"));
+                break;
+
+            case string x when x.Equals("OpenAI", StringComparison.OrdinalIgnoreCase):
+                builder.Services.AddOpenAITextGeneration(this.GetServiceConfig<OpenAIConfig>("OpenAI"));
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    private void ConfigureRetrievalMemoryDb(IKernelMemoryBuilder builder)
+    {
+        switch (this._kernelMemoryConfig.Retrieval.MemoryDbType)
+        {
+            case string x when x.Equals("AzureAISearch", StringComparison.OrdinalIgnoreCase):
+                builder.Services.AddAzureAISearchAsMemoryDb(this.GetServiceConfig<AzureAISearchConfig>("AzureAISearch"));
+                break;
+
+            case string x when x.Equals("Qdrant", StringComparison.OrdinalIgnoreCase):
+                builder.Services.AddQdrantAsMemoryDb(this.GetServiceConfig<QdrantConfig>("Qdrant"));
+                break;
+
+            case string x when x.Equals("Postgres", StringComparison.OrdinalIgnoreCase):
+                builder.Services.AddPostgresAsMemoryDb(this.GetServiceConfig<PostgresConfig>("Postgres"));
+                break;
+
+            case string x when x.Equals("Redis", StringComparison.OrdinalIgnoreCase):
+                builder.Services.AddRedisAsMemoryDb(this.GetServiceConfig<RedisConfig>("Redis"));
+                break;
+
+            case string x when x.Equals("SimpleVectorDb", StringComparison.OrdinalIgnoreCase):
+                builder.Services.AddSimpleVectorDbAsMemoryDb(this.GetServiceConfig<SimpleVectorDbConfig>("SimpleVectorDb"));
+                break;
+
+            case string x when x.Equals("SimpleTextDb", StringComparison.OrdinalIgnoreCase):
+                builder.Services.AddSimpleTextDbAsMemoryDb(this.GetServiceConfig<SimpleTextDbConfig>("SimpleTextDb"));
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    private void ConfigureIngestionMemoryDb(IKernelMemoryBuilder builder)
+    {
+        foreach (var type in this._kernelMemoryConfig.DataIngestion.MemoryDbTypes)
+        {
+            switch (type)
+            {
+                case string x when x.Equals("AzureAISearch", StringComparison.OrdinalIgnoreCase):
+                    {
+                        IMemoryDb instance = this.GetServiceInstance<IMemoryDb>(builder,
+                            services => services.AddAzureAISearchAsMemoryDb(this.GetServiceConfig<AzureAISearchConfig>("AzureAISearch")));
+
+                        builder.AddIngestionMemoryDb(instance);
+
+                        break;
+                    }
+                case string x when x.Equals("Qdrant", StringComparison.OrdinalIgnoreCase):
+                    {
+                        IMemoryDb instance = this.GetServiceInstance<IMemoryDb>(builder,
+                            services => services.AddQdrantAsMemoryDb(this.GetServiceConfig<QdrantConfig>("Qdrant")));
+
+                        builder.AddIngestionMemoryDb(instance);
+                        break;
+                    }
+                case string x when x.Equals("Postgres", StringComparison.OrdinalIgnoreCase):
+                    {
+                        IMemoryDb instance = this.GetServiceInstance<IMemoryDb>(builder,
+                            services => services.AddPostgresAsMemoryDb(this.GetServiceConfig<PostgresConfig>("Postgres")));
+
+                        builder.AddIngestionMemoryDb(instance);
+                        break;
+                    }
+                case string x when x.Equals("Redis", StringComparison.OrdinalIgnoreCase):
+                    {
+                        IMemoryDb instance = this.GetServiceInstance<IMemoryDb>(builder,
+                            services => services.AddRedisAsMemoryDb(this.GetServiceConfig<RedisConfig>("Redis")));
+
+                        builder.AddIngestionMemoryDb(instance);
+                        break;
+                    }
+                case string x when x.Equals("SimpleVectorDb", StringComparison.OrdinalIgnoreCase):
+                    {
+                        IMemoryDb instance = this.GetServiceInstance<IMemoryDb>(builder,
+                            services => services.AddSimpleVectorDbAsMemoryDb(this.GetServiceConfig<SimpleVectorDbConfig>("SimpleVectorDb")));
+
+                        builder.AddIngestionMemoryDb(instance);
+                        break;
+                    }
+                case string x when x.Equals("SimpleTextDb", StringComparison.OrdinalIgnoreCase):
+                    {
+                        IMemoryDb instance = this.GetServiceInstance<IMemoryDb>(builder,
+                            services => services.AddSimpleTextDbAsMemoryDb(this.GetServiceConfig<SimpleTextDbConfig>("SimpleTextDb")));
+
+                        builder.AddIngestionMemoryDb(instance);
+                        break;
+                    }
+                case "":
+                    break;
+                default:
+                    throw new ConfigurationException($"Unknown Memory DB option '{type}'. " +
+                                                    "To use a custom Memory DB, set the configuration value to an empty string, " +
+                                                    "and inject the custom implementation using `IKernelMemoryBuilder.WithCustomMemoryDb(...)`");
+            }
+        }
+    }
+
+    private void ConfigureRetrievalEmbeddingGenerator(IKernelMemoryBuilder builder)
+    {
+        switch (this._kernelMemoryConfig.Retrieval.EmbeddingGeneratorType)
+        {
+            case string x when x.Equals("AzureOpenAI", StringComparison.OrdinalIgnoreCase):
+            case string y when y.Equals("AzureOpenAIEmbedding", StringComparison.OrdinalIgnoreCase):
+                builder.Services.AddAzureOpenAIEmbeddingGeneration(this.GetServiceConfig<AzureOpenAIConfig>("AzureOpenAI") ?? this.GetServiceConfig<AzureOpenAIConfig>("AzureOpenAIEmbedding"));
+                break;
+            case string x when x.Equals("OpenAI", StringComparison.OrdinalIgnoreCase):
+                builder.Services.AddOpenAITextEmbeddingGeneration(this.GetServiceConfig<OpenAIConfig>("OpenAI"));
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void ConfigureSearchClient(IKernelMemoryBuilder builder)
+    {
+        builder.WithSearchClientConfig(this._kernelMemoryConfig.Retrieval.SearchClient);
+    }
+
+    private void ConfigureIngestionEmbeddingGenerators(IKernelMemoryBuilder builder)
+    {
+        if (this._kernelMemoryConfig.DataIngestion.EmbeddingGeneratorTypes.Count > 1)
+        {
+            throw new NotSupportedException("Using multiple embedding generators is currently unsupported. " +
+                                            "You may contact the team if this feature is required, or workaround this exception " +
+                                            "using KernelMemoryBuilder methods explicitly.");
+        }
+
+        foreach (var type in this._kernelMemoryConfig.DataIngestion.EmbeddingGeneratorTypes)
+        {
+            switch (type)
+            {
+                case string x when x.Equals("AzureOpenAI", StringComparison.OrdinalIgnoreCase):
+                case string y when y.Equals("AzureOpenAIEmbedding", StringComparison.OrdinalIgnoreCase):
+                    {
+                        ITextEmbeddingGenerator instance = this.GetServiceInstance<ITextEmbeddingGenerator>(builder,
+                            services => services.AddAzureOpenAIEmbeddingGeneration(this.GetServiceConfig<AzureOpenAIConfig>("AzureOpenAIEmbedding")));
+
+                        builder.AddIngestionEmbeddingGenerator(instance);
+                        break;
+                    }
+                case string x when x.Equals("OpenAI", StringComparison.OrdinalIgnoreCase):
+                    {
+                        ITextEmbeddingGenerator instance = this.GetServiceInstance<ITextEmbeddingGenerator>(builder,
+                            services => services.AddOpenAITextEmbeddingGeneration(this.GetServiceConfig<OpenAIConfig>("OpenAI")));
+
+                        builder.AddIngestionEmbeddingGenerator(instance);
+                        break;
+                    }
+                default:
+                    break;
+            }
+        }
     }
 
     private void ConfigureStorageDependency(IKernelMemoryBuilder builder)
@@ -207,6 +400,25 @@ internal sealed class ServiceConfiguration
 
         this._rawAppSettings = newAppSettings.Build();
         this._kernelMemoryConfig = this._rawAppSettings.GetSection(ConfigRoot).Get<KernelMemoryConfig>()!;
+    }
+
+    private T GetServiceInstance<T>(IKernelMemoryBuilder builder, Action<IServiceCollection> addCustomService)
+    {
+        IServiceCollection services = new ServiceCollection();
+
+        foreach (ServiceDescriptor serviceDescriptor in builder.Services)
+        {
+            if (serviceDescriptor.ServiceType == typeof(T))
+            {
+                continue;
+            }
+
+            services.Add(serviceDescriptor);
+        }
+
+        addCustomService.Invoke(services);
+
+        return services.BuildServiceProvider().GetService<T>() ?? throw new ConfigurationException($"Unable to build {nameof(T)}");
     }
 
     private T GetServiceConfig<T>(string serviceName)
