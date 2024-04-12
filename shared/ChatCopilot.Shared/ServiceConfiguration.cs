a58719924c4d823a8b7bc8ba8c7a64d550b88876
ShuaiHua Du <shuaihua.du@outlook.com>
2024-04-12 16:15:08 +0800

    Configure storage queue dependencies.

--- shared/ChatCopilot.Shared/ServiceConfiguration.cs
+++ shared/ChatCopilot.Shared/ServiceConfiguration.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.KernelMemory;
 using Microsoft.KernelMemory.Configuration;
+using Microsoft.KernelMemory.ContentStorage.DevTools;
+using Microsoft.KernelMemory.Pipeline.Queue.DevTools;
 
 namespace ChatCopilot.Shared;
 
@@ -80,6 +82,60 @@ internal sealed class ServiceConfiguration
         return builder;
     }
 
+    private void ConfigureStorageDependency(IKernelMemoryBuilder builder)
+    {
+        switch (this._kernelMemoryConfig.ContentStorageType)
+        {
+            case string x1 when x1.Equals("AzureBlob", StringComparison.OrdinalIgnoreCase):
+            case string x2 when x2.Equals("AzureBlobs", StringComparison.OrdinalIgnoreCase):
+                builder.Services.AddAzureBlobsAsContentStorage(this.GetServiceConfig<AzureBlobsConfig>("AzureBlob") ?? this.GetServiceConfig<AzureBlobsConfig>("AzureBlobs"));
+                break;
+            case string x when x.Equals("SimpleFileStorage", StringComparison.OrdinalIgnoreCase):
+                builder.Services.AddSimpleFileStorageAsContentStorage(this.GetServiceConfig<SimpleFileStorageConfig>("SimpleFileStorage"));
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void ConfigureQueueDependency(IKernelMemoryBuilder builder)
+    {
+        if (string.Equals(this._kernelMemoryConfig.DataIngestion.OrchestrationType, "Distributed", StringComparison.OrdinalIgnoreCase))
+        {
+            switch (this._kernelMemoryConfig.DataIngestion.DistributedOrchestration.QueueType)
+            {
+                case string x1 when x1.Equals("AzureQueue", StringComparison.OrdinalIgnoreCase):
+                case string x2 when x2.Equals("AzureQueues", StringComparison.OrdinalIgnoreCase):
+
+                    builder.Services.AddAzureQueuesOrchestration(this.GetServiceConfig<AzureQueuesConfig>("AzureQueues") ?? this.GetServiceConfig<AzureQueuesConfig>("AzureQueue"));
+                    break;
+                case string x when x.Equals("RabbitMQ", StringComparison.OrdinalIgnoreCase):
+                    builder.Services.AddRabbitMQOrchestration(this.GetServiceConfig<RabbitMqConfig>("RabbitMq"));
+                    break;
+                case string x when x.Equals("SimpleQueues", StringComparison.OrdinalIgnoreCase):
+                    builder.Services.AddSimpleQueues(this.GetServiceConfig<SimpleQueuesConfig>("SimpleQueues"));
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+    private void ConfigureTextPartitioning(IKernelMemoryBuilder builder)
+    {
+        if (this._kernelMemoryConfig.DataIngestion.TextPartitioning != null)
+        {
+            this._kernelMemoryConfig.DataIngestion.TextPartitioning.Validate();
+
+            builder.WithCustomTextPartitioningOptions(this._kernelMemoryConfig.DataIngestion.TextPartitioning);
+        }
+    }
+
+    private void ConfigureMimeTypeDetectionDependency(IKernelMemoryBuilder builder)
+    {
+        builder.WithDefaultMimeTypeDetection();
+    }
+
     private static IConfiguration ReadAppSettings(string? settingsDirectory)
     {
         ConfigurationBuilder builder = new();
@@ -155,6 +211,6 @@ internal sealed class ServiceConfiguration
 
     private T GetServiceConfig<T>(string serviceName)
     {
-        return this._kernelMemoryConfig.GetServiceConfig<T>(this., serviceName);
+        return this._kernelMemoryConfig.GetServiceConfig<T>(this._rawAppSettings, serviceName);
     }
 }
