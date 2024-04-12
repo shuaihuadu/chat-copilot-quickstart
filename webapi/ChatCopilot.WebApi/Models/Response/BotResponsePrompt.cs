a01600e8d1bcf28957d141f7e33bf1d6f00104c6
ShuaiHua Du <shuaihua.du@outlook.com>
2024-04-13 01:16:19 +0800

    Add chat plugins.

---
+++ webapi/ChatCopilot.WebApi/Models/Response/BotResponsePrompt.cs
@@ -0,0 +1,38 @@
+﻿namespace ChatCopilot.WebApi.Models.Response;
+
+public class BotResponsePrompt
+{
+    [JsonPropertyName("systemPersona")]
+    public string SystemPersona { get; set; } = string.Empty;
+
+    [JsonPropertyName("audience")]
+    public string Audience { get; set; } = string.Empty;
+
+    [JsonPropertyName("userIntent")]
+    public string UserIntent { get; set; } = string.Empty;
+
+    [JsonPropertyName("chatMemories")]
+    public string PastMemories { get; set; } = string.Empty;
+
+    [JsonPropertyName("chatHistory")]
+    public string ChatHistory { get; set; } = string.Empty;
+
+    [JsonPropertyName("metaPromptTemplate")]
+    public ChatHistory MetaPromptTemplate { get; set; } = [];
+
+    public BotResponsePrompt(
+        string systemInstructions,
+        string audience,
+        string userIntent,
+        string chatMemories,
+        string chatHistory,
+        ChatHistory metaPromptTemplate)
+    {
+        this.SystemPersona = systemInstructions;
+        this.Audience = audience;
+        this.UserIntent = userIntent;
+        this.PastMemories = chatMemories;
+        this.ChatHistory = chatHistory;
+        this.MetaPromptTemplate = metaPromptTemplate;
+    }
+}
--- webapi/ChatCopilot.WebApi/Plugins/Chat/ChatPlugin.cs
+++ webapi/ChatCopilot.WebApi/Plugins/Chat/ChatPlugin.cs
@@ -1,5 +1,4 @@
-﻿
-using Microsoft.SemanticKernel.ChatCompletion;
+﻿using Microsoft.SemanticKernel.Connectors.OpenAI;
 
 namespace ChatCopilot.WebApi.Plugins.Chat
 {
@@ -143,7 +142,7 @@ namespace ChatCopilot.WebApi.Plugins.Chat
         private async Task<CopilotChatMessage> GetChatResponseAsync(string chatId, string userId, KernelArguments chatContext, CopilotChatMessage userMessage, CancellationToken cancellationToken)
         {
             //设置System Instruction
-            string? systemInstructions = await AsyncUtils.SafeInvokeAsync(() => this.RenderSystemInstructions(chatId, chatContext, cancellationToken), nameof(RenderSystemInstructions));
+            string systemInstructions = await AsyncUtils.SafeInvokeAsync(() => this.RenderSystemInstructions(chatId, chatContext, cancellationToken), nameof(RenderSystemInstructions));
 
             ChatHistory metaPrompt = new(systemInstructions);
 
@@ -184,9 +183,231 @@ namespace ChatCopilot.WebApi.Plugins.Chat
 
             chatContext[TokenUtils.GetFunctionKey("SystemMetaPrompt")] = TokenUtils.GetContextMessageTokenCount(metaPrompt).ToString(CultureInfo.CurrentCulture);
 
-            BotResponsePrompt promptView = new BotResponsePrompt(systemInstructions, audience, userIntent, memoryText, allowedChatHisotry, metaPrompt);
+            BotResponsePrompt promptView = new(systemInstructions, audience, userIntent, memoryText, allowedChatHisotry, metaPrompt);
 
             return await this.HandleBotResponseAsync(chatId, userId, chatContext, promptView, citationMap.Values.AsEnumerable(), cancellationToken);
+        }
+
+        private async Task<CopilotChatMessage> HandleBotResponseAsync(
+            string chatId,
+            string userId,
+            KernelArguments chatContext,
+            BotResponsePrompt prompt,
+            IEnumerable<CitationSource> citations,
+            CancellationToken cancellationToken)
+        {
+            await this.UpdateBotResponseStatusOnClientAsync(chatId, "Generating bot response", cancellationToken);
+
+            CopilotChatMessage chatMessage = await AsyncUtils.SafeInvokeAsync(
+                () => this.StreamingResponseToClientAsync(chatId, userId, prompt, cancellationToken, citations), nameof(StreamingResponseToClientAsync));
+
+            await this.UpdateBotResponseStatusOnClientAsync(chatId, "Saving message to chat history", cancellationToken);
+            await this._chatMessageRepository.UpsertAsync(chatMessage);
+
+            await this.UpdateBotResponseStatusOnClientAsync(chatId, "Generating semantic chat memory", cancellationToken);
+            await AsyncUtils.SafeInvokeAsync(() => SemanticChatMemoryExtractor.ExtraceSemanticMemoryAsync(
+                    chatId,
+                    this._kernelMemory,
+                    this._kernel,
+                    chatContext,
+                    this._promptOptions,
+                    this._logger,
+                    cancellationToken), nameof(SemanticChatMemoryExtractor.ExtraceSemanticMemoryAsync));
+
+            await this.UpdateBotResponseStatusOnClientAsync(chatId, "Saving token usage", cancellationToken);
+            chatMessage.TokenUsage = this.GetTokenUsage(chatContext, chatMessage.Content);
+
+            await this.UpdateMessageOnClient(chatMessage, cancellationToken);
+            await this._chatMessageRepository.UpsertAsync(chatMessage);
+
+            return chatMessage;
+        }
+
+        private Dictionary<string, int> GetTokenUsage(KernelArguments kernelArguments, string? content = null)
+        {
+            Dictionary<string, int> tokenUsageDict = new Dictionary<string, int>(StringComparer.OrdinalIgnoreCase);
+
+            foreach (string function in TokenUtils.semanticFunctions.Values)
+            {
+                if (kernelArguments.TryGetValue($"{function}TokenUsage", out object? tokenUsage))
+                {
+                    if (tokenUsage is string tokenUsageString)
+                    {
+                        tokenUsageDict.Add(function, int.Parse(tokenUsageString, CultureInfo.InvariantCulture));
+                    }
+                }
+            }
+
+            if (content is not null)
+            {
+                tokenUsageDict.Add(TokenUtils.semanticFunctions["SystemCompletion"]!, TokenUtils.TokenCount(content));
+            }
+
+            return tokenUsageDict;
+        }
+
+        private async Task<CopilotChatMessage> StreamingResponseToClientAsync(
+            string chatId,
+            string userId,
+            BotResponsePrompt prompt,
+            CancellationToken cancellationToken,
+            IEnumerable<CitationSource>? citations = null)
+        {
+            IChatCompletionService chatCompletionService = this._kernel.GetRequiredService<IChatCompletionService>();
+
+            IAsyncEnumerable<StreamingChatMessageContent> stream = chatCompletionService.GetStreamingChatMessageContentsAsync(
+                prompt.MetaPromptTemplate,
+                this.CreateChatRequestSettings(),
+                this._kernel,
+                cancellationToken);
+
+            CopilotChatMessage chatMessage = await this.CreateBotMessageOnClient(chatId, userId, JsonSerializer.Serialize(prompt), string.Empty, cancellationToken, citations);
+
+            await foreach (var contentPiece in stream)
+            {
+                chatMessage.Content += contentPiece;
+
+                await this.UpdateMessageOnClient(chatMessage, cancellationToken);
+            }
+
+            return chatMessage;
+        }
+
+        private async Task UpdateMessageOnClient(CopilotChatMessage chatMessage, CancellationToken cancellationToken)
+        {
+            await this._messageRelayHubContext.Clients.Group(chatMessage.ChatId).SendAsync("ReceiveMessageUpdate", chatMessage, cancellationToken);
+        }
+
+        private async Task<CopilotChatMessage> CreateBotMessageOnClient(
+            string chatId,
+            string userId,
+            string prompt,
+            string content,
+            CancellationToken cancellationToken,
+            IEnumerable<CitationSource>? citations,
+            Dictionary<string, int>? tokenUsage = null)
+        {
+            CopilotChatMessage chatMessage = CopilotChatMessage.CreateBotResponseMessage(chatId, content, prompt, citations, tokenUsage);
+
+            await this._messageRelayHubContext.Clients.Group(chatId).SendAsync("ReceiveMessage", chatId, userId, chatMessage, cancellationToken);
+
+            return chatMessage;
+        }
+
+        private int GetMaxRequestTokenBudget()
+        {
+            const int ExtraOpenAiMessageTokens = 20;
+
+            return this._promptOptions.CompletionTokenLimit
+                - ExtraOpenAiMessageTokens
+                - this._promptOptions.ResponseTokenLimit;
+        }
+
+        private async Task<string> GetUserIntentAsync(KernelArguments context, CancellationToken cancellationToken)
+        {
+            KernelArguments intentContext = new(context);
+
+            int tokenBudget = this._promptOptions.CompletionTokenLimit
+                - this._promptOptions.ResponseTokenLimit
+                - TokenUtils.TokenCount(string.Join("\n", [
+                    this._promptOptions.SystemPersona,
+                    this._promptOptions.SystemIntent,
+                    this._promptOptions.SystemIntentContinuation
+                ]));
+
+            intentContext["tokenLimit"] = tokenBudget.ToString(new NumberFormatInfo());
+            intentContext["knowledgeCutoff"] = this._promptOptions.KnowledgeCutoffDate;
+
+            KernelFunction completionFunction = this._kernel.CreateFunctionFromPrompt(
+                this._promptOptions.SystemIntentExtraction,
+                this.CreateIntentCompletionSettings(),
+                functionName: "UserIntentExtraction",
+                description: "Extract user intent");
+
+            FunctionResult result = await completionFunction.InvokeAsync(this._kernel, intentContext, cancellationToken);
+
+            string? tokenUsage = TokenUtils.GetFunctionTokenUsage(result, this._logger);
+            if (tokenUsage is not null)
+            {
+                context[TokenUtils.GetFunctionKey("SystemIntentExtraction")] = tokenUsage;
+            }
+            else
+            {
+                this._logger.LogError("Unable to determine token usage for userIntentExtraction");
+            }
+
+            return $"User intent: {result}";
+        }
+
+        private async Task<string> GetAudienceAsync(KernelArguments context, CancellationToken cancellationToken)
+        {
+            KernelArguments audienceContext = new(context);
+
+            int historyTokenBudget = this._promptOptions.CompletionTokenLimit
+                - this._promptOptions.ResponseTokenLimit
+                - TokenUtils.TokenCount(string.Join("\n\n", [
+                    this._promptOptions.SystemAudience,
+                    this._promptOptions.SystemAudienceContinuation
+                ]));
+
+            audienceContext["tokenLimit"] = historyTokenBudget.ToString(new NumberFormatInfo());
+
+            KernelFunction completionFunction = this._kernel.CreateFunctionFromPrompt(
+                this._promptOptions.SystemAudienceExtraction,
+                this.CreateIntentCompletionSettings(),
+                functionName: "SystemAudienceExtraction",
+                description: "Extract audience");
+
+            FunctionResult result = await completionFunction.InvokeAsync(this._kernel, audienceContext, cancellationToken);
+
+            string? tokenUsage = TokenUtils.GetFunctionTokenUsage(result, this._logger);
+
+            if (tokenUsage != null)
+            {
+                context[TokenUtils.GetFunctionKey("SystemAudienceExtraction")] = tokenUsage;
+            }
+            else
+            {
+                this._logger.LogError("Unable to determin token usage of audienceExtraction");
+            }
+
+            return $"List of participants: {result}";
+        }
+
+        private OpenAIPromptExecutionSettings CreateChatRequestSettings()
+        {
+            return new OpenAIPromptExecutionSettings
+            {
+                MaxTokens = this._promptOptions.ResponseTokenLimit,
+                Temperature = this._promptOptions.ResponseTemperature,
+                TopP = this._promptOptions.ResponseTopP,
+                FrequencyPenalty = this._promptOptions.ResponseFrequencyPenalty,
+                PresencePenalty = this._promptOptions.ResponsePresencePenalty,
+                ToolCallBehavior = ToolCallBehavior.AutoInvokeKernelFunctions
+            };
+        }
+
+        private OpenAIPromptExecutionSettings CreateIntentCompletionSettings()
+        {
+            return new OpenAIPromptExecutionSettings
+            {
+                MaxTokens = this._promptOptions.ResponseTokenLimit,
+                Temperature = this._promptOptions.IntentTemperature,
+                TopP = this._promptOptions.IntentTopP,
+                FrequencyPenalty = this._promptOptions.IntentFrequencyPenalty,
+                PresencePenalty = this._promptOptions.IntentPresencePenalty,
+                StopSequences = ["] bot:"]
+            };
+        }
+
+        private async Task<string> RenderSystemInstructions(string chatId, KernelArguments context, CancellationToken cancellationToken)
+        {
+            await this.UpdateBotResponseStatusOnClientAsync(chatId, "Initializing prompt", cancellationToken);
+
+            KernelPromptTemplateFactory kernelPromptTemplateFactory = new();
+            IPromptTemplate promptTemplate = kernelPromptTemplateFactory.Create(new PromptTemplateConfig(this._promptOptions.SystemPersona));
+
+            return await promptTemplate.RenderAsync(this._kernel, context, cancellationToken);
         }
 
         private async Task<CopilotChatMessage> SaveNewMessageAsync(string message, string userId, string userName, string chatId, string messageType, CancellationToken cancellationToken)
