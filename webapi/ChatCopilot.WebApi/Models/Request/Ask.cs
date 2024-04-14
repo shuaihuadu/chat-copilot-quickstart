0f2d70a23f99bd7d30f61594ea7a92a052efb359
ShuaiHua Du <shuaihua.du@outlook.com>
2024-04-14 18:09:29 +0800

    Add ask related models.

---
+++ webapi/ChatCopilot.WebApi/Models/Request/Ask.cs
@@ -0,0 +1,9 @@
+﻿namespace ChatCopilot.WebApi.Models.Request;
+
+public class Ask
+{
+    [Required, NotEmptyOrWhitespace]
+    public string Input { get; set; } = string.Empty;
+
+    public IEnumerable<KeyValuePair<string, string>> Variables { get; set; } = [];
+}
---
+++ webapi/ChatCopilot.WebApi/Models/Response/AskResult.cs
@@ -0,0 +1,8 @@
+﻿namespace ChatCopilot.WebApi.Models.Response;
+
+public class AskResult
+{
+    public string Value { get; set; } = string.Empty;
+
+    public IEnumerable<KeyValuePair<string, object?>>? Variables { get; set; } = [];
+}
