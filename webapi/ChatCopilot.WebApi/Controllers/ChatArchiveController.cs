bc69986f965828149cc78cee568ac2a5dd6e7082
ShuaiHua Du <shuaihua.du@outlook.com>
2024-04-14 23:58:29 +0800

    Add chat archive.

---
+++ webapi/ChatCopilot.WebApi/Controllers/ChatArchiveController.cs
@@ -0,0 +1,107 @@
+﻿namespace ChatCopilot.WebApi.Controllers;
+
+[ApiController]
+public class ChatArchiveController : ControllerBase
+{
+    private readonly ILogger<ChatArchiveController> _logger;
+    private readonly IKernelMemory _kernelMemory;
+    private readonly ChatSessionRepository _chatSessionRepository;
+    private readonly ChatMessageRepository _chatMessageRepository;
+    private readonly ChatParticipantRepository _chatParticipantRepository;
+    private readonly ChatArchiveEmbeddingConfig _chatArchiveEmbeddingConfig;
+    private readonly PromptsOptions _promptsOptions;
+
+    public ChatArchiveController(
+        ILogger<ChatArchiveController> logger,
+        IKernelMemory kernelMemory,
+        IOptions<PromptsOptions> promptsOptions,
+        ChatSessionRepository chatSessionRepository,
+        ChatMessageRepository chatMessageRepository,
+        ChatParticipantRepository chatParticipantRepository,
+        ChatArchiveEmbeddingConfig chatArchiveEmbeddingConfig)
+    {
+        this._logger = logger;
+        this._kernelMemory = kernelMemory;
+        this._promptsOptions = promptsOptions.Value;
+
+        this._chatSessionRepository = chatSessionRepository;
+        this._chatMessageRepository = chatMessageRepository;
+        this._chatParticipantRepository = chatParticipantRepository;
+        this._chatArchiveEmbeddingConfig = chatArchiveEmbeddingConfig;
+    }
+
+    [HttpGet]
+    [Route("chats/{chatId:guid}/archive")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [Authorize(Policy = AuthPolicyName.RequireChatParticipant)]
+    public async Task<ActionResult<ChatArchive?>> DownloadAsync(Guid chatId, CancellationToken cancellationToken)
+    {
+        this._logger.LogDebug("Received call to download a chat archive");
+
+        ChatArchive chatArchive = await this.CreateChatArchiveAsync(chatId, cancellationToken);
+
+        return this.Ok(chatArchive);
+    }
+
+    private async Task<ChatArchive> CreateChatArchiveAsync(Guid chatId, CancellationToken cancellationToken)
+    {
+        string chatIdString = chatId.ToString();
+
+        ChatArchive chatArchive = new ChatArchive
+        {
+            EmbeddingConfigurations = this._chatArchiveEmbeddingConfig
+        };
+
+        ChatSession chat = await this._chatSessionRepository.FindByIdAsync(chatIdString);
+
+        chatArchive.ChatTitle = chat.Title;
+
+        chatArchive.SystemDescription = chat.SafeSystemDescription;
+
+        chatArchive.ChatHistory = await this.GetAllChatMessagesAsync(chatIdString);
+
+        foreach (var memory in this._promptsOptions.MemoryMap.Keys)
+        {
+            chatArchive.Embeddings.Add(memory, await this.GetMemoryRecordsAndAppendToEmbeddingsAsync(chatIdString, memory, cancellationToken));
+        }
+
+        chatArchive.DocumentEmbeddings.Add("GlobalDocuments", await this.GetMemoryRecordsAndAppendToEmbeddingsAsync(Guid.Empty.ToString(), this._promptsOptions.DocumentMemoryName, cancellationToken));
+
+        chatArchive.DocumentEmbeddings.Add("ChatDocuments", await this.GetMemoryRecordsAndAppendToEmbeddingsAsync(chatIdString, this._promptsOptions.DocumentMemoryName, cancellationToken));
+
+        return chatArchive;
+    }
+
+    private async Task<List<Citation>> GetMemoryRecordsAndAppendToEmbeddingsAsync(string chatId, string memoryName, CancellationToken cancellationToken)
+    {
+        List<Citation> collectionMemoryRecords;
+
+        try
+        {
+            SearchResult result = await this._kernelMemory.SearchMemoryAsync(
+                this._promptsOptions.MemoryIndexName,
+                query: "*",
+                relevanceThreshold: -1,
+                chatId,
+                memoryName,
+                cancellationToken);
+
+            collectionMemoryRecords = result.Results;
+        }
+        catch (Exception ex) when (!ex.IsCriticalException())
+        {
+            this._logger.LogError(ex, "Cannot search collection {0}", memoryName);
+
+            collectionMemoryRecords = [];
+        }
+
+        return collectionMemoryRecords;
+    }
+
+    private async Task<List<CopilotChatMessage>> GetAllChatMessagesAsync(string chatId)
+    {
+        return (await this._chatMessageRepository.FindByChatIdAsync(chatId)).ToList();
+    }
+}
---
+++ webapi/ChatCopilot.WebApi/Models/Response/ChatArchive.cs
@@ -0,0 +1,18 @@
+﻿namespace ChatCopilot.WebApi.Models.Response;
+
+public class ChatArchive
+{
+    public ChatArchiveSchemaInfo Schema { get; set; } = new();
+
+    public ChatArchiveEmbeddingConfig EmbeddingConfigurations { get; set; } = new();
+
+    public string ChatTitle { get; set; } = string.Empty;
+
+    public string SystemDescription { get; set; } = string.Empty;
+
+    public List<CopilotChatMessage> ChatHistory { get; set; } = [];
+
+    public Dictionary<string, List<Citation>> Embeddings { get; set; } = [];
+
+    public Dictionary<string, List<Citation>> DocumentEmbeddings { get; set; } = [];
+}
